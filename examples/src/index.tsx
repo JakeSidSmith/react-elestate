3658d7d73590e273076d1970ba11dbbe3707a4ca
Jake 'Sid' Smith <jake@dabapps.com>
2021-01-27 21:32:45 +0000

    Improve example import

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import createElevation from '../../src';
+import createElevation from 'react-elestate';
 
 interface ElevatedState {
   count: number;
